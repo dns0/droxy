15095f3a5134645ff51d3b0ceb66f3e7fa4414d9
diraction <diraction>
2017-11-26 13:41:10 +0000

    async client

--- src/resolver/handler.rs
+++ src/resolver/handler.rs
@@ -1,37 +1,68 @@
 use std::net::SocketAddr;
+use std::cell::RefCell;
 
+use futures::future;
 use futures::Future;
 use futures::IntoFuture;
 
+use tokio_core::reactor::Handle;
 use trust_dns::error::*;
 use trust_dns::op::message::Message;
 use trust_dns::op::Query;
 use trust_dns::udp::UdpClientConnection;
+use trust_dns::udp::UdpClientStream;
 use trust_dns::tcp::TcpClientConnection;
 use trust_dns::client::{Client, SyncClient};
+use trust_dns::client::ClientFuture;
+use trust_dns::client::BasicClientHandle;
+use trust_dns::client::ClientHandle;
 use trust_dns_server::server::Request;
 
 pub struct SmartResolver {
     conn: SyncClient,
     tcp_client: SyncClient,
+    fut_client: RefCell<BasicClientHandle>,
 }
 
 impl SmartResolver {
-    pub fn new(sa: SocketAddr)-> Result<SmartResolver, ClientError> {
+    pub fn new(sa: SocketAddr, handle: Handle)-> Result<SmartResolver, ClientError> {
         let client = UdpClientConnection::new(sa);
         let client: UdpClientConnection = client?;
         let client =SyncClient::new(client);
         let tcpconn = TcpClientConnection::new(sa)?;
         let tcpclient = SyncClient::new(tcpconn);
+
+        let (streamfut, streamhand) = UdpClientStream::new(sa, &handle.clone());
+        let futclient = ClientFuture::new(streamfut, streamhand, &handle.clone(), None);
         Ok(SmartResolver {
             conn: client,
             tcp_client: tcpclient,
+            fut_client: RefCell::new(futclient),
         })
     }
 
     pub fn handle_future(&self, req: &Request, use_tcp:bool) -> Box<Future<Item=Message, Error=ClientError>> {
-        let res = self.handle_request(req, use_tcp);
-        Box::new(res.into_future())
+        let queries = req.message.queries();
+        if queries.len() != 1 {
+            panic!("more than 1 queries in a message: {:?}", req.message);
+        }
+        let q: &Query = &queries[0];
+        let name = q.name();
+        println!("querying {:?}", name);
+        let id = req.message.id();
+
+        let res = self.fut_client.borrow_mut()
+            .query(name.clone(), q.query_class(), q.query_type());
+        let m
+        = res.and_then(move|result| {
+            let mut msg = Message::new();
+            msg.set_id(id);
+            for ans in result.answers() {
+                msg.add_answer(ans.clone());
+            }
+            future::ok(msg)
+        });
+        Box::new(m)
     }
 
     pub fn handle_request(&self, req: &Request, use_tcp: bool) -> Result<Message, ClientError> {
