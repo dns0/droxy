a8e4b806f6f8fc74a5eacb2d9374f395a19ee872
diraction <diraction>
2017-11-26 00:48:25 +0000

    enable tcp

--- src/resolver/mod.rs
+++ src/resolver/mod.rs
@@ -2,6 +2,7 @@
 
 use std::net;
 use std::str::FromStr;
+use std::time::Duration;
 
 use tokio_core::reactor::Handle;
 
@@ -12,6 +13,9 @@ pub fn start_resolver(h: Handle){
     let sa = net::SocketAddr::new (net::IpAddr::from_str("8.8.8.8").unwrap(), 53);
     let handler = handler::SmartResolver::new(sa);
     let server = server_future::ServerFuture::new(handler).unwrap();
-    let udpsock = net::UdpSocket::bind("127.0.0.1:7863").unwrap();
+    let listenaddr: net::SocketAddr = "127.0.0.1:7863".parse().unwrap();
+    let udpsock = net::UdpSocket::bind(listenaddr).unwrap();
     server.listen_udp(udpsock,h.clone());
+    let tcp = net::TcpListener::bind(listenaddr).unwrap();
+    server.listen_tcp(tcp, Duration::from_secs(5), h.clone());
 }
