e6af64d6862a0cd5fe3b05b8683e2c7228c7a81f
diraction <diraction>
2017-11-26 17:12:25 +0000

    no reuse of tcp client

--- src/resolver/handler.rs
+++ src/resolver/handler.rs
@@ -20,7 +20,8 @@ use std::time::Duration;
 
 pub struct SmartResolver {
     fut_client: RefCell<BasicClientHandle>,
-    fut_tcp: RefCell<BasicClientHandle>,
+    server: SocketAddr,
+    handle: Handle,
 }
 
 impl SmartResolver {
@@ -29,12 +30,17 @@ impl SmartResolver {
         let (streamfut, streamhand) = UdpClientStream::new(sa, &handle.clone());
         let futclient = ClientFuture::new(streamfut, streamhand, &handle.clone(), None);
 
-        let (streamfut, streamhand) = TcpClientStream::new(sa, &handle.clone());
-        let futtcp = ClientFuture::new(streamfut, streamhand, &handle.clone(), None);
         Ok(SmartResolver {
             fut_client: RefCell::new(futclient),
-            fut_tcp: RefCell::new(futtcp),
+            server: sa,
+            handle: handle,
         })
+    }
+
+    fn get_tcp_client(&self)-> BasicClientHandle {
+        let (streamfut, streamhand) = TcpClientStream::new(self.server, &self.handle.clone());
+        let futtcp = ClientFuture::new(streamfut, streamhand, &self.handle.clone(), None);
+        futtcp
     }
 
     pub fn handle_future(&self, req: &Request, use_tcp:bool) -> Box<Future<Item=Message, Error=ClientError>> {
@@ -50,9 +56,12 @@ impl SmartResolver {
         let timer = Timer::default();
         let timeout = timer.sleep(Duration::from_secs(5));
 
-        let client = if use_tcp { &self.fut_tcp } else { &self.fut_client };
-        let res = client.borrow_mut()
-            .query(name.clone(), q.query_class(), q.query_type());
+        let res = if use_tcp {
+            let mut c = self.get_tcp_client();
+            c.query(name.clone(), q.query_class(), q.query_type())
+        } else {
+            self.fut_client.borrow_mut().query(name.clone(), q.query_class(), q.query_type())
+        };
         let m
         = res.and_then(move|result| {
             let mut msg = Message::new();
